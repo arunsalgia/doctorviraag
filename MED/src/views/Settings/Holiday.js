f30479bf5ac830bd669fe762b791d17d97d01f1a
AS <arunsalgia@gmail.com>
2022-01-12 17:38:23 +0530

    patient login disabled

--- MED/src/views/Settings/Holiday.js
+++ MED/src/views/Settings/Holiday.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState, useContext } from 'react';
 import axios from "axios";
 import TextField from '@material-ui/core/TextField';
-import { InputAdornment, makeStyles, Container, CssBaseline } from '@material-ui/core';
+import { InputAdornment, Container, CssBaseline } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
 import IconButton from '@material-ui/core/IconButton';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
